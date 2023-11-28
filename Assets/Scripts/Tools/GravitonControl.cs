4d0db4c561aa81cd5f5fe429b6bb75a163940d71
Wesley Williams <wtwill190@gmail.com>
2023-11-27 21:31:23 -0600

    Fixing Graviton Control Script and adding graviton functionality
    Graviton now functions:
    Pulls in bodies
    Follows ship
    
    Still needs to:
    properly deactivate and reactivate gravity for bodies that are pulled in to or exit the field

--- Assets/Scripts/Tools/GravitonControl.cs
+++ Assets/Scripts/Tools/GravitonControl.cs
@@ -9,49 +9,81 @@ public class GravitonControl : MonoBehaviour
     //create state if being active
     private bool GravitonOn = false;
     //talk to the different components
-    public Rigidbody2D rb;
-    public Vector3 velocity;
     public float GravForce = 10;
-    public float radius = 5f;
+    public float radius = 3.18f;
 
 
     //talk to the particle system for VFX
     public ParticleSystem particalEffects;
 
-    void Start()
-    {
-        particalEffects.
-    }
+    //list for keeping track of bodies in the graviton field
+    private List<CelestialBody> affectedBodies = new List<CelestialBody>();
 
     // Update is called once per frame
     void Update()
     {
+        if(!GravitonOn)
+            particalEffects.Stop();
         //space bar to activate
         if(Input.GetKeyDown(KeyCode.Space))
         {
             GravitonOn = !GravitonOn;
             if(GravitonOn)
+            {
                 Debug.Log("Graviton Turned ON");
+                particalEffects.Play();
+            }
             else
+            {
                 Debug.Log("Graviton Turned OFF");
+                particalEffects.Stop();
+                ReactivateGravity();
+            }
+                
         }
-
-        //for spinning the VFX
+        
+        //what happens when the Graviton is turned on
         if(GravitonOn)
         {
-            particalEffects.Play();
+            //for spinning VFX
             transform.Rotate(new Vector3(0, 0, 45 * Time.deltaTime));
+
+            //attraction logic
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+            foreach (Collider2D col in colliders)
+            {
+                if (col.CompareTag("Planet") || col.CompareTag("Debris"))
+                {
+                    CelestialBody celestialBody = col.GetComponent<CelestialBody>();
+                    if (celestialBody != null)
+                    {
+                        if(!affectedBodies.Contains(celestialBody))
+                        {
+                            affectedBodies.Add(celestialBody);
+                            celestialBody.is_gravity_affected = false;
+                        }
+
+                        Rigidbody2D body = celestialBody.rigbod;
+                        if(body != null)
+                        {
+                            Vector2 direction = GravPoint.position - col.transform.position;
+                            body.AddForce(direction.normalized * GravForce);
+                        }
+                    }
+                }
+            }
         }
         
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    //method for reactivating gravity upon exit or graviton field
+    void ReactivateGravity()
     {
-        if (other.CompareTag("Planet || Debris"))
+        foreach (CelestialBody body in affectedBodies)
         {
-            Vector2 direction = GravPoint.position - other.transform.position;
-            other.attachedRigidbody.AddForce(direction.normalized * attractionForce);
+            body.is_gravity_affected = true;
         }
+        affectedBodies.Clear();
     }
 
     void OnDrawGizmosSelected()
