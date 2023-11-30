505cf6c4afd02ecdf3e06de430730a5fe27305f1
Mgiordantx <mgiordantx@gmail.com>
2023-11-29 22:02:37 -0500

    fixed debris add mass

--- Assets/Scripts/Body/Debris.cs
+++ Assets/Scripts/Body/Debris.cs
@@ -17,16 +17,17 @@ public class Debris : CelestialBody
         {
             //Debug.Log("hit planet...");
             // add mass to other rigidbody
-            collision.otherRigidbody.mass += 1;
+            collision.gameObject.GetComponent<Rigidbody2D>().mass += 1;
+            collision.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0);
             // Debug.Log(collision.otherRigidbody.mass);
             // Debug.Log(collision.relativeVelocity.magnitude);
             Destroy(gameObject);
         }
         else if(collision.gameObject.tag == "Singularity")
         {
-            collision.otherRigidbody.mass += 1;
-            collision.gameObject.transform.localScale += new Vector3(rigbod.mass/(10*Mathf.Sqrt(rigbod.mass)), rigbod.mass/(10*Mathf.Sqrt(rigbod.mass)), 0);
-            //Debug.Log("hit THE SINGULARITY...");
+            collision.gameObject.GetComponent<Rigidbody2D>().mass += 1;
+            collision.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+            Debug.Log("DEBRIS hit THE SINGULARITY...");
             Destroy(gameObject);
         }
     }
